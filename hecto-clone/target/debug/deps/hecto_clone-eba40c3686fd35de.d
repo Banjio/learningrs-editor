47933881c33e2aa53873fe70406e598466c37804
Maximilian Beier <beiersmaxi@gmail.com>
2023-03-27 22:44:19 +0200

    Removed target

--- hecto-clone/target/debug/deps/hecto_clone-eba40c3686fd35de.d
+++
@@ -1,5 +0,0 @@
-/home/maxbeier/ws/learningrs-editor/hecto-clone/target/debug/deps/hecto_clone-eba40c3686fd35de: src/main.rs
-
-/home/maxbeier/ws/learningrs-editor/hecto-clone/target/debug/deps/hecto_clone-eba40c3686fd35de.d: src/main.rs
-
-src/main.rs:
--- hecto-clone/target/debug/hecto-clone.d
+++
@@ -1,1 +0,0 @@
-/home/maxbeier/ws/learningrs-editor/hecto-clone/target/debug/hecto-clone: /home/maxbeier/ws/learningrs-editor/hecto-clone/src/main.rs
