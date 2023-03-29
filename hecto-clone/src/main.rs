72ea303335f68c43feced853bd6b2e97cddc638f
Maximilian Beier <beiersmaxi@gmail.com>
2023-03-29 22:08:44 +0200

    Continuing tutorial

--- hecto-clone/src/main.rs
+++ hecto-clone/src/main.rs
@@ -1,9 +1,23 @@
-use std::io::{self, Read};
+use std::io::{self, stdout, Read};
+use termion::raw::IntoRawMode;
 
 fn main() {
+    let _stdout = stdout().into_raw_mode().unwrap();
+
     for b in io::stdin().bytes(){
-        // let c:u8 = b.unwrap(); -> If you do it like this the integer representations of chars are returned
-        let c:char = b.unwrap() as char;
-        println!("{}", c);
+        let b = b.unwrap();
+        // As => primitive to single (byte to in this case char)
+        let c:char = b as char;
+        // Test wheter a character is a control character, based on ASCII Representations
+        if c.is_control(){
+            // {} is for character known how to print and {:?} is for not known characters which may have a debug representation
+            // \r is for carriage return -> Printing the output line by line
+            println!("{:?} \r", b)
+        } else {
+            println!("{:?} ({}) \r", b, c)
+        }
+        if c == 'q' {
+            break;
+        }
     }
 }
