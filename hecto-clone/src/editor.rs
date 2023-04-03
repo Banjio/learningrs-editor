6057426395c700bfa9754b6f19d3c365cd561afc
Banjio @Maximilian Beier <beiermaximilian13@gmail.com>
2023-04-04 00:31:16 +0200

    Draw tildes to screen

--- hecto-clone/src/editor.rs
+++ hecto-clone/src/editor.rs
@@ -1,9 +1,10 @@
-use std::io::{self, stdout};
+use std::io::{self, stdout, Write};
 use termion::event::Key;
 use termion::input::TermRead;
 use termion::raw::IntoRawMode;
 
 fn die(e: std::io::Error) {
+    print!("{}", termion::clear::All);
     panic!("{}", e);
 }
 
@@ -19,13 +20,35 @@ impl Editor{
         into_raw_mode().
         unwrap();
         loop{
-            if let Err(error) = self.process_keypress(){
+            if let Err(error) = self.refresh_screen() {
                 die(error);
             }
             if self.should_quit {
                 break;
             }
+
+            if let Err(error) = self.process_keypress() {
+                die(error);
+            }
         }
+    }
+
+    fn refresh_screen(&self) -> Result<(), std::io::Error> {
+        //We use print to write 4 bytes. The first byte is x1b = Escape character. => This initiates an escape sequence followed by instuctions what to do with the escape sequence
+        //print!("\x1b[2j");
+        // Using termion we can achieve the same
+        print!("{}", termion::clear::All);
+        // After clearing the cursor will be at the bottom of the screen this will set it to the top again
+        print!("{}", termion::cursor::Goto(1, 1));
+        // Print a goodbye messages in case the user leaves
+        // Remember that if we get an error or leave, we once finally refresh the screen => hence we can print it here 
+        if self.should_quit {
+            println!("Goodbye, Mate.\r")
+        } else {
+            self.draw_rows();
+            print!("{}", termion::cursor::Goto(1,1));
+        }
+        io::stdout().flush()
     }
 
     fn process_keypress(&mut self) -> Result<(), std::io::Error>{
@@ -35,6 +58,12 @@ impl Editor{
             _ => (),
         }
         Ok(())
+    }
+
+    fn draw_rows(&self) {
+        for _ in 0..24 {
+            println!("~r");
+        }
     }
 
     fn read_key() -> Result<Key, std::io::Error> {
